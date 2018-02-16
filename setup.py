da55e19c31e89d197f34a3482feca8fe961fd9fd
nielstron <n.muendler@web.de>
2018-02-16 12:39:24 +0100

    Corrected reuirements settings

--- setup.py
+++ setup.py
@@ -14,18 +14,18 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='PySyncThru',
-      version='0.1.1',
+      version='0.1.2',
       description='Automated JSON API based communication with Samsung SyncThru Web Service',
       author='Niels Mündler',
       author_email='n.muendler@web.de',
       url='https://github.com/nielstron/pysyncthru/',
       py_modules=['pysyncthru'],
       packages = find_packages(),
-      requires=[
+      install_requires = [
           'demjson',
           'requests',
         ],
-      long_description=long_description,
+      long_description = long_description,
       license='MIT',
       classifiers=[
         'Development Status :: 3 - Alpha',

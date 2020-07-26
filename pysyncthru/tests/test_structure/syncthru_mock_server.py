545008ef2f2f229b225a9f78b3999a4704b1ef47
Niels Mündler <n.muendler@tum.de>
2020-07-26 09:36:44 +0200

    Enforce formatting

--- pysyncthru/tests/test_structure/syncthru_mock_server.py
+++ pysyncthru/tests/test_structure/syncthru_mock_server.py
@@ -1,6 +1,7 @@
 import os
 import urllib.parse
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+
 try:
     from http import HTTPStatus
 except ImportError:
@@ -9,7 +10,7 @@ except ImportError:
 import posixpath
 from pathlib import Path
 
-SERVER_DIR = Path(__file__).parent or Path('.')
+SERVER_DIR = Path(__file__).parent or Path(".")
 
 
 class SyncThruServer(HTTPServer):
@@ -24,13 +25,12 @@ class SyncThruServer(HTTPServer):
 
 
 class SyncThruRequestHandler(SimpleHTTPRequestHandler):
-    
     def do_GET(self):
         if self.server.blocked:
             self.send_error(403, "Access denied because server blocked")
         else:
             super(SyncThruRequestHandler, self).do_GET()
-    
+
     def translate_path(self, path):
         """Translate a /-separated PATH to the local filename syntax.
 
@@ -41,16 +41,16 @@ class SyncThruRequestHandler(SimpleHTTPRequestHandler):
         only slightly changed method of the standard library
         """
         # abandon query parameters
-        path = path.split('?', 1)[0]
-        path = path.split('#', 1)[0]
+        path = path.split("?", 1)[0]
+        path = path.split("#", 1)[0]
         # Don't forget explicit trailing slash when normalizing. Issue17324
-        trailing_slash = path.rstrip().endswith('/')
+        trailing_slash = path.rstrip().endswith("/")
         try:
-            path = urllib.parse.unquote(path, errors='surrogatepass')
+            path = urllib.parse.unquote(path, errors="surrogatepass")
         except UnicodeDecodeError:
             path = urllib.parse.unquote(path)
         path = posixpath.normpath(path)
-        words = path.split('/')
+        words = path.split("/")
         words = filter(None, words)
         path = str(SERVER_DIR.absolute())
         for word in words:
@@ -59,7 +59,7 @@ class SyncThruRequestHandler(SimpleHTTPRequestHandler):
                 continue
             path = os.path.join(path, word)
         if trailing_slash:
-            path += '/'
+            path += "/"
         return path
 
     def send_error(self, code, message=None, explain=None):
@@ -72,23 +72,25 @@ class SyncThruRequestHandler(SimpleHTTPRequestHandler):
         """
         self.log_error("code %d, message %s", code, message)
         self.send_response(code, message)
-        self.send_header('Connection', 'close')
+        self.send_header("Connection", "close")
 
         # Message body is omitted for cases described in:
         #  - RFC7230: 3.3. 1xx, 204(No Content), 304(Not Modified)
         #  - RFC7231: 6.3.6. 205(Reset Content)
         body = None
-        if (code >= 200 and
-                code not in (HTTPStatus.NO_CONTENT, HTTPStatus.RESET_CONTENT,
-                             HTTPStatus.NOT_MODIFIED)):
+        if code >= 200 and code not in (
+            HTTPStatus.NO_CONTENT,
+            HTTPStatus.RESET_CONTENT,
+            HTTPStatus.NOT_MODIFIED,
+        ):
             # HTML encode to prevent Cross Site Scripting attacks
             # (see bug #1100201)
             # Specialized error method for fronius
-            with SERVER_DIR.joinpath(".error.html").open('rb') as file:
+            with SERVER_DIR.joinpath(".error.html").open("rb") as file:
                 body = file.read()
             self.send_header("Content-Type", self.error_content_type)
-            self.send_header('Content-Length', int(len(body)))
+            self.send_header("Content-Length", int(len(body)))
         self.end_headers()
 
-        if self.command != 'HEAD' and body:
+        if self.command != "HEAD" and body:
             self.wfile.write(body)
--- pysyncthru/tests/test_web.py
+++ pysyncthru/tests/test_web.py
@@ -15,7 +15,7 @@ import asyncio
 from pysyncthru import SyncThru
 from .web_raw.web_state import RAW
 
-ADDRESS = 'localhost'
+ADDRESS = "localhost"
 
 
 class SyncthruWebTest(unittest.TestCase):
@@ -23,7 +23,7 @@ class SyncthruWebTest(unittest.TestCase):
     server = None
     server_control = None
     port = 0
-    url = 'http://localhost:80'
+    url = "http://localhost:80"
     syncthru = None
 
     def setUp(self):
@@ -62,140 +62,119 @@ class SyncthruWebTest(unittest.TestCase):
         self.assertTrue(self.syncthru.is_online())
 
     def test_status_sleeping(self):
-        self.assertEqual(self.syncthru.device_status(), 'Sleeping')
+        self.assertEqual(self.syncthru.device_status(), "Sleeping")
 
     def test_model(self):
-        self.assertEqual(self.syncthru.model(), RAW['identity']['model_name'])
+        self.assertEqual(self.syncthru.model(), RAW["identity"]["model_name"])
 
     def test_toner_filter(self):
         self.assertEqual(
             self.syncthru.toner_status(True),
-            {'black': {
-                'opt': 1,
-                'remaining': 58,
-                'cnt': 229,
-                'newError': ''
-            }})
+            {"black": {"opt": 1, "remaining": 58, "cnt": 229, "newError": ""}},
+        )
 
     def test_toner_no_filter(self):
-        empty = {'opt': 0, 'remaining': 0, 'cnt': 0, 'newError': ''}
+        empty = {"opt": 0, "remaining": 0, "cnt": 0, "newError": ""}
         self.assertEqual(
-            self.syncthru.toner_status(False), {
-                'yellow': empty,
-                'magenta': empty,
-                'cyan': empty,
-                'black': {
-                    'opt': 1,
-                    'remaining': 58,
-                    'cnt': 229,
-                    'newError': ''
-                }
-            })
+            self.syncthru.toner_status(False),
+            {
+                "yellow": empty,
+                "magenta": empty,
+                "cyan": empty,
+                "black": {"opt": 1, "remaining": 58, "cnt": 229, "newError": ""},
+            },
+        )
 
     def test_input_tray_filter(self):
         self.assertEqual(
-            self.syncthru.input_tray_status(True), {
+            self.syncthru.input_tray_status(True),
+            {
                 1: {
-                    'capa': 150,
-                    'newError': '',
-                    'opt': 1,
-                    'paper_size1': 4,
-                    'paper_size2': 0,
-                    'paper_type1': 2,
-                    'paper_type2': 0
+                    "capa": 150,
+                    "newError": "",
+                    "opt": 1,
+                    "paper_size1": 4,
+                    "paper_size2": 0,
+                    "paper_type1": 2,
+                    "paper_type2": 0,
                 }
-            })
+            },
+        )
 
     def test_input_tray_no_filter(self):
         self.assertEqual(
-            self.syncthru.input_tray_status(False), {
+            self.syncthru.input_tray_status(False),
+            {
                 1: {
-                    'capa': 150,
-                    'newError': '',
-                    'opt': 1,
-                    'paper_size1': 4,
-                    'paper_size2': 0,
-                    'paper_type1': 2,
-                    'paper_type2': 0
+                    "capa": 150,
+                    "newError": "",
+                    "opt": 1,
+                    "paper_size1": 4,
+                    "paper_size2": 0,
+                    "paper_type1": 2,
+                    "paper_type2": 0,
                 },
                 2: {
-                    'capa': 0,
-                    'newError': '',
-                    'opt': 0,
-                    'paper_size1': 0,
-                    'paper_size2': 0,
-                    'paper_type1': 2,
-                    'paper_type2': 0
+                    "capa": 0,
+                    "newError": "",
+                    "opt": 0,
+                    "paper_size1": 0,
+                    "paper_size2": 0,
+                    "paper_type1": 2,
+                    "paper_type2": 0,
                 },
                 3: {
-                    'capa': 0,
-                    'newError': '',
-                    'opt': 0,
-                    'paper_size1': 0,
-                    'paper_size2': 0,
-                    'paper_type1': 2,
-                    'paper_type2': 0
+                    "capa": 0,
+                    "newError": "",
+                    "opt": 0,
+                    "paper_size1": 0,
+                    "paper_size2": 0,
+                    "paper_type1": 2,
+                    "paper_type2": 0,
                 },
                 4: {
-                    'capa': 0,
-                    'newError': '',
-                    'opt': 0,
-                    'paper_size1': 0,
-                    'paper_size2': 0,
-                    'paper_type1': 2,
-                    'paper_type2': 0
-                }
-            })
+                    "capa": 0,
+                    "newError": "",
+                    "opt": 0,
+                    "paper_size1": 0,
+                    "paper_size2": 0,
+                    "paper_type1": 2,
+                    "paper_type2": 0,
+                },
+            },
+        )
 
     def test_output_tray(self):
-        self.assertEqual(self.syncthru.output_tray_status(),
-                         {0: {
-                             'capacity': 100,
-                             'name': 1,
-                             'status': ''
-                         }})
+        self.assertEqual(
+            self.syncthru.output_tray_status(),
+            {0: {"capacity": 100, "name": 1, "status": ""}},
+        )
 
     def test_drum_status_filter(self):
         self.assertEqual(self.syncthru.drum_status(True), {})
 
     def test_drum_status_no_filter(self):
         self.assertEqual(
-            self.syncthru.drum_status(False), {
-                'black': {
-                    'newError': '',
-                    'opt': 0,
-                    'remaining': 0
-                },
-                'cyan': {
-                    'newError': '',
-                    'opt': 0,
-                    'remaining': 100
-                },
-                'magenta': {
-                    'newError': '',
-                    'opt': 0,
-                    'remaining': 100
-                },
-                'yellow': {
-                    'newError': '',
-                    'opt': 0,
-                    'remaining': 100
-                }
-            })
+            self.syncthru.drum_status(False),
+            {
+                "black": {"newError": "", "opt": 0, "remaining": 0},
+                "cyan": {"newError": "", "opt": 0, "remaining": 100},
+                "magenta": {"newError": "", "opt": 0, "remaining": 100},
+                "yellow": {"newError": "", "opt": 0, "remaining": 100},
+            },
+        )
 
     def test_location(self):
-        self.assertEqual(self.syncthru.location(), RAW['identity']['location'])
+        self.assertEqual(self.syncthru.location(), RAW["identity"]["location"])
 
     def test_serial_number(self):
-        self.assertEqual(self.syncthru.serial_number(),
-                         RAW['identity']['serial_num'])
+        self.assertEqual(self.syncthru.serial_number(), RAW["identity"]["serial_num"])
 
     def test_hostname(self):
-        self.assertEqual(self.syncthru.hostname(),
-                         RAW['identity']['host_name'])
+        self.assertEqual(self.syncthru.hostname(), RAW["identity"]["host_name"])
 
     def test_cap(self):
-        self.assertEqual(self.syncthru.capability(), RAW['capability'])
+        self.assertEqual(self.syncthru.capability(), RAW["capability"])
 
     def tearDown(self):
         self.server_control.stop_server()
@@ -207,7 +186,7 @@ class NonSyncthruWebTest(unittest.TestCase):
     server = None
     server_control = None
     port = 0
-    url = 'http://localhost:80'
+    url = "http://localhost:80"
     syncthru = None
 
     def test_no_syncthru(self):
@@ -239,6 +218,7 @@ class NonSyncthruWebTest(unittest.TestCase):
         self.server.set_blocked()
 
         try:
+
             async def fetch():
                 async with aiohttp.ClientSession() as session:
                     self.syncthru = SyncThru(self.url, session)
@@ -246,7 +226,9 @@ class NonSyncthruWebTest(unittest.TestCase):
 
             loop = asyncio.get_event_loop()
             loop.run_until_complete(fetch())
-            self.fail("No error thrown when noticing that the host does not support Syncthru")
+            self.fail(
+                "No error thrown when noticing that the host does not support Syncthru"
+            )
         except ValueError:
             pass
 

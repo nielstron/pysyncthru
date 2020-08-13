80c1b10df3abc7d9340b4c5d3550e03962b93fb5
Niels Mündler <n.muendler@tum.de>
2020-08-13 14:42:03 +0200

    Bump to version 0.7.0
    This change includes some heavy refactoring. Language dependency is scrapped, rather than that now the more reliable hrDeviceStatus value is used. The display information (statusX) is concatenated and returned on demand via device_status_details

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ short_description = "{}".format(
 
 setup(
     name="PySyncThru",
-    version="0.6.0",
+    version="0.7.0",
     description=short_description,
     author="nielstron",
     author_email="n.muendler@web.de",

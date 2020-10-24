a2b74fcf9bd7bbdf28fb568c630503771aa2e692
Niels Mündler <n.muendler@tum.de>
2020-10-24 03:32:22 +0200

    Bump to new patch version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ short_description = "{}".format(
 
 setup(
     name="PySyncThru",
-    version="0.7.1",
+    version="0.7.2",
     description=short_description,
     author="nielstron",
     author_email="n.muendler@web.de",

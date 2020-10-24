71e8fb9fea7a92b4fe423713aa8aea354ed58e83
Ville Skyttä <ville.skytta@iki.fi>
2020-10-24 04:14:06 +0300

    Dist fixes (#45)
    * Remove no longer applicable py_modules from setup
    
    * Fix .error.html inclusion in package data
    
    * Don't install tests

--- setup.py
+++ setup.py
@@ -21,15 +21,10 @@ setup(
     author="nielstron",
     author_email="n.muendler@web.de",
     url="https://github.com/nielstron/pysyncthru/",
-    py_modules=["pysyncthru"],
-    packages=find_packages(),
+    packages=find_packages(exclude=("pysyncthru.tests", "pysyncthru.tests.*")),
     package_data={
         "pysyncthru": [
             "py.typed",
-        ],
-        "pysyncthru.tests.test_structure": [
-            "sws/app/information/home/home.json",
-            "*.html",
         ],
     },
     install_requires=[

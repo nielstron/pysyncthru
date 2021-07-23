14b09c4e82611c8814db731323814d2e54856a0f
Niels Mündler <n.muendler@tum.de>
2021-07-23 11:02:45 +0200

    New patch version (0.7.4)
    - include meaning of "mp"
    - start automatic source tagging

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ short_description = "{}".format(
 
 setup(
     name="PySyncThru",
-    version="0.7.3",
+    version="0.7.4",
     description=short_description,
     author="nielstron",
     author_email="n.muendler@web.de",

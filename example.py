346e127eecb6779610242d9011b5726faa8c82f0
Ville Skyttä <ville.skytta@iki.fi>
2021-12-11 23:01:55 +0200

    example: pretty-format all data for readability

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ Copyright (c) 2017-2018 Fabian Affolter <fabian@affolter-engineering.ch>
 Licensed under MIT. All rights reserved.
 """
 import asyncio
+import pprint
 import sys
 
 import aiohttp
@@ -29,7 +30,7 @@ async def main(ip: str) -> None:
             # Get the details about a tray
             print("Tray 1 Capacity:", printer.input_tray_status()["tray_1"]["capa"])
         # Print all available details from the printer
-        print("All data:\n", printer.raw())
+        print("All data:\n", pprint.pformat(printer.raw()))
 
 if len(sys.argv) != 2:
     print(f"Usage: {__file__} IP-ADDRESS", file=sys.stderr)
